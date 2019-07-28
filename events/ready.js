ec8e2fd6528bbf57b901127b033a7c45ae2a41be
jestecdoan <52170298+jestecdoan@users.noreply.github.com>
2019-07-28 19:35:01 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]  Koruma Calisiyor.`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Sunucu Aktif!`);
   client.user.setStatus("online");
-  client.user.setGame(`${prefix} | noonbot.tk`);
+  client.user.setGame(`${prefix} | remzibot.tk`);
   console.log(`Hazir Sunucuyu Baslatin...`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Noon.`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Sunucu Aktif!`);
